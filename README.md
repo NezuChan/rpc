a1dbc3ddd37defbec691ae89d55990d5b246c956
KChi <59391215+KagChi@users.noreply.github.com>
2022-05-22 15:11:07 +0700

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<a href="https://github.com/NezuChan/rpc"> <img align="right" src="https://i.kagchi.my.id/nezuko.png" width="150"></a>
+
+> Promote NezukoChan by promoting her in your presence !
+<br>
+
+[![Nezuko Community](https://discordapp.com/api/guilds/785715968608567297/embed.png?style=banner2)](https://discord.gg/8hmbMUCVMD)
+
+## Author
+NezuChan RPC © [KagChi](https://github.com/KagChi)
+Authored and maintained by KagChi.
+> Github [KagChi](https://github.com/KagChi)
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { Client } from "discord-rpc";
+const client = new Client({ transport: "ipc" });
+
+client.on("ready", async () => {
+    await client.setActivity({
+        details: "Your Favourite Discord Bot !",
+        state: "Supports BiliBili and many more !",
+        largeImageText: "Listen to your favorite tunes !",
+        largeImageKey: "nezu-image",
+        startTimestamp: Date.now(),
+        buttons: [
+            {
+                label: "Invite NezukoChan",
+                url: "https://nezu.my.id"
+            },
+            { label: "Join Support Server", url: "https://discord.gg/8hmbMUCVMD" }
+        ]
+    });
+});
+
+void client.login({ clientId: "616169470293049344" });
